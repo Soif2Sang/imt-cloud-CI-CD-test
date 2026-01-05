d00a0771da9fad738c97d845716a3170eff4fda6
soif2sang <sirefight.gaming@gmail.com>
2026-01-05 23:46:17 +0100

    return a static version in hello word

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ items_db: List[Item] = [
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple!",
+        "message": "Bienvenue sur l'API d'exemple! v1",
         "docs": "/docs",
         "health": "/health"
     }
