435889090ce0f58c58a6f38f5ce024acf1db5b05
soif2sang <sirefight.gaming@gmail.com>
2026-01-06 16:04:09 +0100

    Bring back good docker-compose

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ items_db: List[Item] = [
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple! v5.7",
+        "message": "Bienvenue sur l'API d'exemple! v5.8",
         "docs": "/docs",
         "health": "/health"
     }
