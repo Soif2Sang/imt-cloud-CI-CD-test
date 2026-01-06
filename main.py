27ecbe7b07c670b3cdfb044ac5dd52e35125da68
soif2sang <sirefight.gaming@gmail.com>
2026-01-07 00:22:08 +0100

    Make it so the deployment is sucessfull then fails (test rollback feature)

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ ITEM_NOT_FOUND_MESSAGE = "Item non trouvé"
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple! v5.10",
+        "message": "Bienvenue sur l'API d'exemple! v5.11",
         "docs": "/docs",
         "health": "/health"
     }
