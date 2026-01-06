92dc0962ec6ac500ac1c3d3402d89f36291e6a7e
soif2sang <sirefight.gaming@gmail.com>
2026-01-06 09:52:57 +0100

    bump to v4

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ items_db: List[Item] = [
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple! v3",
+        "message": "Bienvenue sur l'API d'exemple! v4",
         "docs": "/docs",
         "health": "/health"
     }
