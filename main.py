6aa582698431d086cecaa8b34a98f451975aab9f
soif2sang <sirefight.gaming@gmail.com>
2026-01-06 12:36:19 +0100

    push

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ items_db: List[Item] = [
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple! v5.1",
+        "message": "Bienvenue sur l'API d'exemple! v5.2",
         "docs": "/docs",
         "health": "/health"
     }
