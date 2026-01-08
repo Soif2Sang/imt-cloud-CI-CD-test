fbb71957f3a6830bfc67c2fc0ac56b70e3d149c9
soif2sang <sirefight.gaming@gmail.com>
2026-01-08 17:37:19 +0100

    bump v5.12

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ ITEM_NOT_FOUND_MESSAGE = "Item non trouvé"
 async def root():
     """Endpoint racine"""
     return {
-        "message": "Bienvenue sur l'API d'exemple! v5.12",
+        "message": "Bienvenue sur l'API d'exemple! v5.13",
         "docs": "/docs",
         "health": "/health"
     }
